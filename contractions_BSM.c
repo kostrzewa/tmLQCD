50a4d9c1cae9fdc05daed0de5113ea4d945ce07e
Ferenc Pittler <pittler@hiskp.uni-bonn.de>
2021-04-12 16:59:52 +0300

    including a factor of 0.5 multiplying c_sw

--- contractions_BSM.c
+++ contractions_BSM.c
@@ -445,7 +445,11 @@ int main(int argc, char *argv[]){
               else {
                 init_D_psi_BSM3();
                 init_sw_fields(VOLUME);
-                sw_term( (const su3**) g_smeared_gauge_field, 1.,  csw_BSM);
+
+                //Note here the factor of 1/2. has been applied since
+                //the routine assign_mul_one_sw_pm_imu_site_lexic computes
+                //1+i *csw*\sum_{\mu,nu} \sigma_mu,nuF_mu,nu/2.
+                sw_term( (const su3**) g_smeared_gauge_field, 1.,  csw_BSM/2.);
               }
               operator_list[op_id].prop_zero=(bispinor  **)malloc(sizeof(bispinor*)*48);
               if (operator_list[op_id].prop_zero == NULL){
